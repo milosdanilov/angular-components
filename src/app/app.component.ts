fe765d3bf9fb2c7e42111e3df6a56231805e80fd
Milos Danilov <milosdanilov@gmail.com>
2021-12-07 00:48:19 +0100

    list item description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,22 @@ import { SelectListItemDefinition } from './select-list/select-list.interface';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   listItems: SelectListItemDefinition[] = [
-    { name: 'First checkbox', value: false },
-    { name: 'Second checkbox', value: true },
-    { name: 'Third checkbox', value: false }
+    {
+      name: 'First checkbox',
+      value: false,
+      description: 'With support text underneath to add more detail',
+    },
+    {
+      name: 'Second checkbox',
+      value: true,
+      description: 'Some other text goes here',
+    },
+    {
+      name: 'Third checkbox',
+      value: false,
+      description: 'And we end with another snippet of text',
+    },
   ];
 
   onSelectedChange(event: SelectListItemDefinition[]) {
--- src/app/select-list/select-list.interface.ts
+++ src/app/select-list/select-list.interface.ts
@@ -1,4 +1,5 @@
 export interface SelectListItemDefinition {
   name: string;
   value: boolean;
+  description: string;
 }
