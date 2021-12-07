c0e5811127fc1ca8dc104c3a7606ae17494aae13
Milos Danilov <milosdanilov@gmail.com>
2021-12-07 01:12:02 +0100

    simple select list - filter

--- src/app/select-list/select-list.component.ts
+++ src/app/select-list/select-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { SelectListItemDefinition } from './select-list.interface';
 
@@ -7,12 +8,28 @@ import { SelectListItemDefinition } from './select-list.interface';
   templateUrl: './select-list.component.html',
   styleUrls: ['./select-list.component.scss'],
 })
-export class SelectListComponent {
+export class SelectListComponent implements OnInit {
   @Input()
   items: SelectListItemDefinition[] = [];
 
+  @Input()
+  allowFilter = false;
+
   @Output()
   selectedChange = new EventEmitter<SelectListItemDefinition[]>();
+
+  searchTerm: string = '';
+  search$ = new Subject<string>();
+
+  filteredItems!: SelectListItemDefinition[];
+
+  ngOnInit() {
+    this.filteredItems = this.items;
+
+    this.search$.subscribe(term => {
+      this.filteredItems = this.items.filter(item => item.name.toLowerCase().includes(term));
+    });
+  }
 
   onListChange() {
     const selected = this.items.filter((item) => !!item.value);
